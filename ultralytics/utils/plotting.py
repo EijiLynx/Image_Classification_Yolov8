a5518539947138e6c22c89354c771790a5a52fb4
EijiLynx <williamkwakyegyimah@gmail.com>
2023-08-11 13:46:07 +0000

    added-(header,bg-color,box-color,text_description)

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -25,7 +25,7 @@ class Colors:
 
     def __init__(self):
         """Initialize colors as hex = matplotlib.colors.TABLEAU_COLORS.values()."""
-        hexs = ('FF3838', 'FF9D97', 'FF701F', 'FFB21D', 'CFD231', '48F90A', '92CC17', '3DDB86', '1A9334', '00D4BB',
+        hexs = ('FF0000', 'ADD8E6', 'F8C8DC', 'FFA500', '800080', '48F90A', '92CC17', '3DDB86', '1A9334', '00D4BB',
                 '2C99A8', '00C2FF', '344593', '6473FF', '0018EC', '8438FF', '520085', 'CB38FF', 'FF95C8', 'FF37C7')
         self.palette = [self.hex2rgb(f'#{c}') for c in hexs]
         self.n = len(self.palette)
